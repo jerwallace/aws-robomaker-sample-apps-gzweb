d260717e831f4fb4289823b50d5b250c01b00acb
Jeremy Wallace <jeremygw@amazon.com>
2020-05-09 16:33:40 -0400

    clean start

--- start.sh
+++ start.sh
@@ -8,11 +8,11 @@ touch model.config
 ln -s ../../meshes meshes 
 
 DIR=/root/gzweb
-mkdir -p $DIR/http/client/assets
+mkdir -p $DIR/http/client/assets/models
 cd $DIR
 ./get_local_models.py $DIR/http/client/assets
 ./webify_models_v2.py $DIR/http/client/assets
-mkdir $DIR/http/client/assets/models && mv $DIR/http/client/assets/aws* $DIR/http/client/assets/models/
+mv $DIR/http/client/assets/aws* $DIR/http/client/assets/models/
 
 source /sim_app/setup.bash && roslaunch $ROS_PACKAGE_SIM $ROS_LAUNCH_FILE_SIM &>/dev/null & npm start
 #source /robot_app/setup.bash && roslaunch $ROS_PACKAGE_ROBOT $ROS_LAUNCH_FILE_ROBOT &>/dev/null &
