ae0074d042fd14f65bf154bd8b65e3ccc2474dc4
Jeremy Wallace <jeremygw@amazon.com>
2020-05-15 08:59:28 -0400

    small fixes

--- gzweb-docker/start.sh
+++ gzweb-docker/start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo $GAZEBO_MODEL_PATH
+echo $GAZEBO_MASTER_URI
 DIR=/root/gzweb
 mkdir -p $DIR/http/client/assets/models
 cd $DIR
 ./get_local_models.py $DIR/http/client/assets
 ./webify_models_v2.py $DIR/http/client/assets
 mv $DIR/http/client/assets/aws* $DIR/http/client/assets/models/
-npm start
+npm 
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##======= CHANGE ME (OR COMMENT THIS SECTION OUT AND USE ENV VARS) ========##
-HOME=~/aws-robomaker-sample-apps-gzweb
+HOME=~/Documents/Git/aws-robomaker-sample-apps-gzweb
 WORKSPACE_DIR=aws-robomaker-sample-application-cloudwatch
 WORLDS=(aws_robomaker_bookstore_world aws_robomaker_small_house_world)
 export GZWEB_DOCKER_IMAGE=gzweb:latest
@@ -14,29 +14,33 @@ export TURTLEBOT3_MODEL=waffle_pi
 export ROBOT_APP_INSTALL=$HOME/$WORKSPACE_DIR/robot_ws
 export SIM_APP_INSTALL=$HOME/$WORKSPACE_DIR/simulation_ws
 ##==========================##
-shell=false
 
+detached=false
+
+# Build the code in a container and ensure XML is valid for any visual DAE files in the source.
 build() {
     docker run -v $SIM_APP_INSTALL:/simulation_ws $ROS_DOCKER_IMAGE bash -c "cd /simulation_ws && rosws update && rosdep install --from-paths src --ignore-src -r -y && colcon build"
     docker run -v $ROBOT_APP_INSTALL:/robot_ws $ROS_DOCKER_IMAGE bash -c "cd /robot_ws && rosws update && rosdep install --from-paths src --ignore-src -r -y && colcon build"
     python fixme.py $SIM_APP_INSTALL/install
 }
 
+# This will load the Turtlebot and Sample World models into GZWeb.
 create_model_path() {
     TB3_DIR=/simulation_ws/install/turtlebot3_description_reduced_mesh/share/turtlebot3_description_reduced_mesh
     # Add the paths to your model files for each world that is included in your ROS app.
     for i in "${WORLDS[@]}"
     do
     : 
-        model_path=$model_path:/simulation_ws/install/$i/share/$i/models/
+        model_path=$model_path:/simulation_ws/install/$i/share/$i/models
     done
         model_path=$model_path:$TB3_DIR/models
-        if [ ! -f "$TB3_DIR/models/turtlebot3_description_reduced_mesh/model.config" ]; then
-            touch $TB3_DIR/models/turtlebot3_description_reduced_mesh/model.config
-            ln -s $TB3_DIR/meshes $TB3_DIR/models/turtlebot3_description_reduced_mesh/meshes 
+        if [ ! -f "$HOME/$WORKSPACE_DIR$TB3_DIR/models/turtlebot3_description_reduced_mesh/model.config" ]; then
+            touch $HOME/$WORKSPACE_DIR$TB3_DIR/models/turtlebot3_description_reduced_mesh/model.config
+            ln -s $HOME/$WORKSPACE_DIR$TB3_DIR/meshes $HOME/$WORKSPACE_DIR$TB3_DIR/models/turtlebot3_description_reduced_mesh/meshes 
         fi
 }
 
+# Command line arguments
 while test $# -gt 0
 do
     case "$1" in
@@ -47,8 +51,8 @@ do
         --build) 
             build
             ;;
-        --shell) 
-            shell=true
+        -d) 
+            detached=true
             ;;
         --*) echo "Bad option provided: $1"
             ;;
@@ -58,23 +62,24 @@ do
     shift
 done
 
+# The sample apps connect with AWS services. This ensures there are credentials passed to the container so the app can connect to the cloud.
 if [ -z "$AWS_ACCESS_KEY_ID" ]; then
     echo "Please set your environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY to run these apps."
     exit 
 fi
 
-# Build the code and ensure XML is valid for any visual DAE files in the source.
+# If there is a built application, go ahead and run the containers.
 if [[ -d "$ROBOT_APP_INSTALL" ]] && [[ -d "$SIM_APP_INSTALL" ]]; then
 
     create_model_path
-    export SIM_APP_MODEL_PATHS=$model_path
+    export SIM_APP_MODEL_PATHS=${model_path#?}
 
-    if $shell ; then
-        echo "Starting apps with shell..."
-        docker-compose up
-    else
-        echo "Starting apps without shell..."
+    if $detached ; then
+        echo "Running containers detached. "
         docker-compose up -d
+    else
+        echo "Running containers..."
+        docker-compose up
     fi
 else
     echo "No built applications to run. Try again with --build."
